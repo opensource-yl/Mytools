5ade8f21fb82f719cf1269f48588ef2ac8911a8e
opensource-yl <liuyu3210@gmail.com>
2014-11-19 17:54:47 +0800

    Delete get.sh

--- pxe/pxe.conf/get.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ifconfig eth0 |grep -w "inet addr" |awk '{print $2}' | awk -F : 'BEGIN{ORS=" "}''{print $2}' >>/media/nfsly/addr.conf && ifconfig eth0 |grep HWaddr |awk '{print $5}' >>/media/nfsly/addr.conf
